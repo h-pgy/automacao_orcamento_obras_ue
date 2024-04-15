042fc6acf3263271e873a36741eef4964293b718
h-pgy <hpougy@gmail.com>
2024-04-15 19:54:40 -0300

    env para etl

--- config.py
+++ config.py
@@ -23,17 +23,30 @@ def load_env():
     copy_dot_env_example()
     load_dotenv()
 
-def load_env_var(varname:str)->Union[int, float, str]:
+def load_env_var(varname:str, type:any=None)->Union[int, float, str]:
 
     try:
-        return os.environ[varname]
+        var = os.environ[varname]
+        if type is not None:
+            try:
+                var = type(var)
+            except ValueError:
+                raise RuntimeError(f'Variavel de ambiente {varname} não está no tipo especificado: {type}')
+        return var
     except KeyError:
         raise RuntimeError(f'Variavel de ambiente {varname} não definida.')
     
 load_env()
 
+
+#configs api
 SOF_API_TOKEN=load_env_var('SOF_API_TOKEN')
 SOF_API_HOST=load_env_var('SOF_API_HOST')
 SOF_API_VERSION=load_env_var('SOF_API_VERSION')
 
+#configs de parseamento do proc
 PROC_REGEX_PATT=re.compile(load_env_var('PROC_REGEX_PATT'))
+
+#configs de leitura do excel
+ROWS_TO_SKIP=load_env_var('ROWS_TO_SKIP', int)
+COL_PROC_REGEX_PATT=load_env_var('ROWS_TO_SKIP')
